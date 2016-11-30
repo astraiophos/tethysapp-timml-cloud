55f3e12464b59d21d8db60db297151b275a7d1f2
Jacob <user.name>
2016-11-30 14:29:55 -0700

    Finished basic HTML and CSS

---
+++ tethysapp/wellhead/public/js/table_of_contents.js
@@ -0,0 +1,38 @@
+    createLayerListItem = function (position, layerIndex, layerId, resType, geomType, layerAttributes, visible, layerName, bandInfo, resId, publicFilename, disableChkbx) {
+        var $newLayerListItem;
+        var chkbxHtml;
+        if (disableChkbx === true) {
+            chkbxHtml = '<input class="chkbx-layer" type="checkbox" disabled>';
+        } else {
+            chkbxHtml = '<input class="chkbx-layer" type="checkbox">';
+        }
+        var listHtmlString =
+            '<li class="ui-state-default" ' +
+            'data-layer-index="' + layerIndex + '" ' +
+            'data-layer-id="' + layerId + '" ' +
+            'data-res-id="' + resId + '" ' +
+            'data-res-type="' + resType + '" ' +
+            'data-geom-type="' + geomType + '" ' +
+            'data-public-fname="' + publicFilename + '" ' +
+            'data-layer-attributes="' + layerAttributes + '" ' +
+            'data-band-variable="' + (bandInfo ? bandInfo.variable : undefined) + '" ' +
+            'data-band-units="' + (bandInfo ? bandInfo.units : undefined) + '" ' +
+            'data-band-min="' + (bandInfo ? bandInfo.min : undefined) + '" ' +
+            'data-band-max="' + (bandInfo ? bandInfo.max : undefined) + '" ' +
+            'data-band-nd="' + (bandInfo ? bandInfo.nd : undefined) + '">' +
+            chkbxHtml +
+            '<span class="layer-name">' + layerName + '</span>' +
+            '<input type="text" class="edit-layer-name hidden" value="' + layerName + '">' +
+            '<div class="hmbrgr-div"><img src="/static/hydroshare_gis/images/hamburger-menu.svg"></div>' +
+            '</li>';
+
+        if (position === 'prepend') {
+            $currentLayersList.prepend(listHtmlString);
+            $newLayerListItem = $currentLayersList.find('li:first-child');
+        } else {
+            $currentLayersList.append(listHtmlString);
+            $newLayerListItem = $currentLayersList.find(':last-child');
+        }
+
+        $newLayerListItem.find('.chkbx-layer').prop('checked', visible);
+    };
