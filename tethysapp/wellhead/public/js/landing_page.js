023d156230cfa71bf536fe464f7faf3399b4c275
astraiophos <familyoffullertons@yahoo.com>
2017-01-30 19:25:53 -0700

    Finished building the Mainframe of the display, next to build the tables dynamically from the app and user workspaces. Then, it's formatting after that!

--- tethysapp/wellhead/public/js/landing_page.js
+++ tethysapp/wellhead/public/js/landing_page.js
@@ -12,19 +12,93 @@
  *                              Functions
  *****************************************************************************/
 
-
+var build_user_table;
+var build_example_table;
+var build_layout;
 
 /*****************************************************************************
- *                             Variables
+ *                              Variables
  *****************************************************************************/
 
+var $innerApp;
+var $exampleModels;
+var $userModels;
 
 /*****************************************************************************
- *                              Main Script
+ *                             Main Script
  *****************************************************************************/
 //  Populates the tables with the user and app workspace saved models
 build_user_table = function(){
 
+};
+
+build_example_table = function(){
+
+};
+
+/*****************************************************************************
+ *                            Main Script
+ *****************************************************************************/
+
+build_layout = function(){
+    $innerApp = $('#inner-app-content');
+    $exampleModels = $('#example-models');
+    $userModels = $('#user-models');
+
+    config = {
+        settings:{
+            showPopoutIcon: false,
+            showMaximiseIcon: false,
+            showCloseIcon: false
+        },
+        content: [{
+            type: 'stack',
+            content:[{
+                type: 'component',
+                componentName: 'User Models',
+                componentState: { myId: 'user_models_layout' },
+                isClosable: false
+            },
+            {
+                type: 'component',
+                componentName: 'Example Models',
+                componentState: { myId: 'example_models_layout' },
+                isClosable: false
+            }]
+        }]
+    };
+
+    //  To resize the layout to fit
+    window_height = $(window).height();
+    $innerApp.height(window_height-220);
+
+    myLayout = new GoldenLayout( config,$innerApp );
+    myLayout.registerComponent( 'User Models', function( container, componentState ){
+        container.getElement().addClass('id');
+        container.getElement().attr('id',componentState.myId);
+    });
+        myLayout.registerComponent( 'Example Models', function( container, componentState ){
+        container.getElement().addClass('id');
+        container.getElement().attr('id',componentState.myId);
+    });
+    myLayout.init();
+
+    $userLayoutDiv = $('#user_models_layout');
+    $exampleLayoutDiv = $('#example_models_layout');
+
+    $userModels.appendTo($userLayoutDiv);
+    $exampleModels.appendTo($exampleLayoutDiv);
+};
+
+/*****************************************************************************
+ *                             UI Functions
+ *****************************************************************************/
+initialize_listeners = function(){
+    //  Make model highlight when clicked on
+    $('.model').on('click',function(){
+        $('.model').removeClass('ui-selected');
+        $(this).addClass('ui-selected').trigger('select_change');
+    });
 };
 
 /*****************************************************************************
@@ -38,5 +112,7 @@ $(document).ready(function(){
         //  Build model lists as tables
         build_user_table();
         build_example_table();
+        build_layout();
+        initialize_listeners();
     }
 });
